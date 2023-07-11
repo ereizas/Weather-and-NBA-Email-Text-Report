c986697b9e4c9ad08473d784df1027b64fd2b6a4
Eric Reizas <ereizas@gmail.com>
2023-07-11 12:30:31 -0400

    Added documentation

--- email1.py
+++ email1.py
@@ -7,21 +7,36 @@ import keys_and_passwords
 class email1:
     def __init__(self):
         self.recipients = dict()
-        #ENTER CORRESPONDING INFO FOR EMAIL YOU PLAN TO SET UP TO SEND INFO
-        self.senderInfo = {'email':"wanbareport@gmail.com",'password':keys_and_passwords.password}
+        self.senderInfo = {'email':keys_and_passwords.emailAddr,'password':keys_and_passwords.password}
         self.rList = [recipient for recipient in self.recipients]
 
     def changeRecipients(self,r):
+        """
+        Changes the dictionary self.recipients
+        @param r : value to reassign self.recipients to
+        @return self.recipients : new value of self.recipients
+        """
+
         self.recipients=r
         return self.recipients
 
     def sendEmail(self):
+        """
+        Logs in to email and sends an email with requested info to each recipient
+        """
+
         yag = yagmail.SMTP(self.senderInfo['email'],self.senderInfo['password'])
+        #retrieve all-encompassing data here
         for recipient in self.recipients:
             yag.send(to=recipient,subject="Weather and/or NBA Report for " + str(date.today()) + ":\n\n",contents = self.format(recipient))
 
     #individual formatting
     def format(self,recipient):
+        """
+        Adds information that the user requested to the contents of the email
+        @param recpient : email address for a user
+        """
+
         text = ""
         if self.recipients[recipient][0][0] and content.getScores(self.recipients[recipient][1]):
             text+=content.getScores(self.recipients[recipient][1])
