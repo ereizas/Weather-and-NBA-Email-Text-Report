5d164f44938b3ff9c63175523ef555332e483e37
Eric Reizas <ereizas@gmail.com>
2023-07-11 11:59:46 -0400

    Added documentation and edited legit zipcode determination for efficiency

--- content.py
+++ content.py
@@ -2,13 +2,19 @@ import time
 import requests
 import content_jsons
 import keys_and_passwords
-def getScores(teams):
+def getScores(teams:list[str])->str:
+	"""
+	Returns a string with scores from yesterday for certain teams
+	@param teams : list of str names of teams
+	@return : str informing user of scores of teams they prefer from yesterday or a message saying otherwise
+	"""
 	scores = []
 	teamsPlayed = []
 	if(len(content_jsons.teamsPlayed)!=0):
 		res = "Yesterday's Scores: \n"
 		for t in range(len(content_jsons.teamsPlayed)):
 			if(content_jsons.teamsPlayed[t][0] in teams or content_jsons.teamsPlayed[t][1] in teams):
+				#appends array pair of the two teams that faced off and another two element array of the respective scores
 				teamsPlayed.append(content_jsons.teamsPlayed[t])
 				scores.append(content_jsons.scores[t])
 		for t in range(len(teamsPlayed)):
@@ -27,7 +33,12 @@ def getScores(teams):
 
 #print(getScores(["76ers","Bulls","Trail Blazers","Raptors","Bucks","Nets","Suns"]))
 
-def getSchedule(teams):
+def getSchedule(teams:list[str])->str:
+	"""
+	Returns a string with the schedule for today for teams in teams
+	@param teams : list of str team names
+	@return : str informing user of the schedule for today for teams they prefer or a message saying otherwise
+	"""
 	teamsPlaying = []
 	playTimes = []
 	if len(content_jsons.teamsPlaying)>0:
@@ -46,16 +57,17 @@ def getSchedule(teams):
 
 #print(getSchedule(["76ers","Bulls","Trail Blazers","Raptors","Bucks","Nets","Suns"]))
 
-def getHourlyForecast(unitsInd,zipcodes):
-	#makes sure that any illegitamate zipcodes are removed so that the binary search while loop doesn't become infinite
-	allZips = [i[0] for i in content_jsons.rows]
+
+def getHourlyForecast(unitsInd:int,zipcodes:list[str])->str:
+	"""
+	Returns a string with hourly forecast information for preferred zipcodes
+	@param unitsInd : index into units array to determine which unit of temperature out of Fahrenheit, Celsius, and Kelvin the user wants
+	@param zipcodes : list of str zipcodes the user wants the weather for
+	"""
 	#boolean array that indicates the zipcode in the same index in the zipcodes array is valid
-	legitZipLst = [(j in allZips) for j in zipcodes]
-	if not all(legitZipLst):
-		for i in range(len(zipcodes)-1,-1,-1):
-			if not legitZipLst[i]:
-				zipcodes.remove(zipcodes[i])
+	zipcodes = [zipcode for zipcode in zipcodes if zipcode in [i[0] for i in content_jsons.rows]]
 	coord = [[] for i in zipcodes]
+	#extract coordinates for certain zipcodes via binary search
 	for z in range(len(zipcodes)):
 		left = 0
 		middle = int(len(content_jsons.rows)/2)
@@ -90,26 +102,25 @@ def getHourlyForecast(unitsInd,zipcodes):
 		currentWindSp = data["current"]["wind_speed"]
 		#formats string evenly
 		res += "Weather for zipcode " + zipcodes[coord.index(c)] +": \n\nSunrise & Sunset:\nSunrise: " + sunrise + "\nSunset: " +sunset + "\n\n"+"Current conditions: \nCurrent Temperature: " + str(currentTemp) + " degrees Fahrenheit" + "\nFeels like: " +str(currentFeel) +" degrees Fahrenheit\nCurrent condition: " + currentDesc + "\nUV index: " + str(currentUVI) + "\nWind Speed: " + str(currentWindSp) + " mph" + "\n\n" 
-		#program starts running at 8:00 am and gives the current forecast for the day
 		#finds temperature, feels like temp, percent chance of percipitation, condition, uvi index, and wind speed for each hour
+		hours = 14
 		if unitsInd==0:
-			tempArr = [str(int(data["hourly"][i]["temp"])) + " degrees Fahrenheit" for i in range(14)]
-			feelArr=[str(int(data["hourly"][i]["feels_like"])) + " degrees Fahrenheit" for i in range(14)]
+			tempArr = [str(int(data["hourly"][i]["temp"])) + " degrees Fahrenheit" for i in range(hours)]
+			feelArr=[str(int(data["hourly"][i]["feels_like"])) + " degrees Fahrenheit" for i in range(hours)]
 		elif unitsInd==1:
-			tempArr = [str(int(data["hourly"][i]["temp"])) + " degrees Celsius" for i in range(14)]
-			feelArr=[str(int(data["hourly"][i]["feels_like"])) + " degrees Celsius" for i in range(14)]
+			tempArr = [str(int(data["hourly"][i]["temp"])) + " degrees Celsius" for i in range(hours)]
+			feelArr=[str(int(data["hourly"][i]["feels_like"])) + " degrees Celsius" for i in range(hours)]
 		else:
-			tempArr = [str(int(data["hourly"][i]["temp"])) + " degrees Kelvin" for i in range(14)]
-			feelArr=[str(int(data["hourly"][i]["feels_like"])) + " degrees Kelvin" for i in range(14)]
-		percPrecArr = [str(100*data["hourly"][i]["pop"]) + "%" for i in range(14)]
-		condArr=[data["hourly"][i]["weather"][0]["description"] for i in range(14)]
-		uviArr=[data["hourly"][i]["uvi"] for i in range(14)]
+			tempArr = [str(int(data["hourly"][i]["temp"])) + " degrees Kelvin" for i in range(hours)]
+			feelArr=[str(int(data["hourly"][i]["feels_like"])) + " degrees Kelvin" for i in range(hours)]
+		percPrecArr = [str(100*data["hourly"][i]["pop"]) + "%" for i in range(hours)]
+		condArr=[data["hourly"][i]["weather"][0]["description"] for i in range(hours)]
+		uviArr=[data["hourly"][i]["uvi"] for i in range(hours)]
 		if unitsInd==0:
-			windSpArr = [str(data["hourly"][i]["wind_speed"]) + " mph" for i in range(14)]
+			windSpArr = [str(data["hourly"][i]["wind_speed"]) + " mph" for i in range(hours)]
 		else:
-			windSpArr = [str(data["hourly"][i]["wind_speed"]) + " km/h" for i in range(14)]
-		
-		for i in range(14):
+			windSpArr = [str(data["hourly"][i]["wind_speed"]) + " km/h" for i in range(hours)]
+		for i in range(hours):
 			res += time.strftime("%H:%M", time.localtime(int(data["hourly"][i]["dt"]))) + '\n'
 			res+="Temperature: " + tempArr[i] + "\nFeels like: " + feelArr[i] + "\n"
 			res+="Weather: " + condArr[i] + "\nChance of precipitation: " + percPrecArr[i] + "\n"
@@ -117,7 +128,7 @@ def getHourlyForecast(unitsInd,zipcodes):
 		res += "\n\n\n"
 	return res
 
-#print(getHourlyForecast(0,["18976","19122"]))
+print(getHourlyForecast(0,["18976","19122"]))
 
 if __name__ == '__main__':
 	pass
